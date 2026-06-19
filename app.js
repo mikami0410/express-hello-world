e32b0995c2873e50c144949e5eb20f980770266f
mikami0410 <24fi109@ms.dendai.ac.jp>
2026-06-19 12:29:32 +0900

    ?

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.ws('/ws', (ws, req) => {
     connects.forEach((socket) => {
       if (socket.readyState === 1) {
         // Check if the connection is open
+        massage += "!"
         socket.send(message)
       }
     })
